b0107ac2e85d415216adabddfbe92f66c2ce5028
Kaustubh patil <pkaustubh401@gmail.com>
2022-01-30 12:50:58 +0530

    fixed the text selection bug, made changes in navbar and added buttons on home screen

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("connected");
+
+var typed = new Typed('.role', {
+    strings: ["Front-End Web Devloper", "Front-End Web Devloper"],
+    typeSpeed: 50,
+    backSpeed: 40,
+    loop:false
+  });
